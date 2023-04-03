c57be960131c81ea2182c81ec41a4686dba4ea21
Matthew <logicprojectsforfpgas@gmail.com>
2023-04-03 16:23:46 -0600

    character controller polish

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ fn player_gravity(
     time: Res<Time>,
 ) {
     let float_gravity = -450.0;
-    let true_gravity = -950.0;
+    let true_gravity = -1500.0;
     for mut velocity in player.iter_mut() {
         if keyboard.pressed(KeyCode::Space) {
             velocity.velocity += Vec2::new(0.0, float_gravity * time.delta_seconds());
@@ -66,7 +66,8 @@ fn player_control(
     keyboard: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
-    let player_accel = 300.0;
+    let player_accel = 600.0;
+    let player_deccel = 450.0;
     let player_max_velocity = 225.0;
     for mut velocity in player.iter_mut() {
         if keyboard.pressed(KeyCode::A) {
@@ -77,7 +78,8 @@ fn player_control(
         }
         if !keyboard.pressed(KeyCode::A) && !keyboard.pressed(KeyCode::D) {
             //TODO time dependent slow down?
-            velocity.velocity *= Vec2::new(0.9, 1.0);
+            velocity.velocity.x +=
+                -player_deccel * velocity.velocity.x.signum() * time.delta_seconds();
         }
 
         velocity.velocity.x = velocity
@@ -91,8 +93,14 @@ fn player_jump(
     mut controllers: Query<(&KinematicCharacterControllerOutput, &mut PlayerVelocity)>,
     keyboard: Res<Input<KeyCode>>,
 ) {
-    let jump_strength = 400.0;
+    let jump_strength = 320.0;
     for (controller, mut velocity) in controllers.iter_mut() {
+        if controller.desired_translation.y - controller.effective_translation.y > 0.1 {
+            velocity.velocity.y = -0.1;
+        }
+        if (controller.desired_translation.x - controller.effective_translation.x).abs() > 0.1 {
+            velocity.velocity.x = 0.0;
+        }
         if controller.grounded {
             velocity.velocity.y = -0.1;
             if keyboard.just_pressed(KeyCode::Space) {
@@ -123,7 +131,7 @@ fn setup(mut commands: Commands, assets: Res<AssetServer>) {
     commands.spawn((
         CharacterBundle::new(Vec3::new(-3.0, 0.0, CHARACTER_Z), Character::Player),
         RigidBody::KinematicPositionBased,
-        Collider::ball(15.5),
+        Collider::capsule(Vec2::new(0.0, -6.3), Vec2::new(0.0, 2.5), 20.0 / 2.0),
         PlayerVelocity {
             velocity: Vec2::ZERO,
             last_grounded: 0,
@@ -145,6 +153,22 @@ fn setup(mut commands: Commands, assets: Res<AssetServer>) {
     commands
         .spawn(Collider::cuboid(500.0, 50.0))
         .insert(TransformBundle::from(Transform::from_xyz(0.0, -100.0, 0.0)));
+
+    commands
+        .spawn(Collider::cuboid(48.0, 16.0))
+        .insert(TransformBundle::from(Transform::from_xyz(-114.0, 0.0, 0.0)));
+
+    commands
+        .spawn(Collider::cuboid(32.0, 16.0))
+        .insert(TransformBundle::from(Transform::from_xyz(128.0, 0.0, 0.0)));
+
+    commands
+        .spawn(Collider::cuboid(16.0, 16.0))
+        .insert(TransformBundle::from(Transform::from_xyz(16.0, 128.0, 0.0)));
+
+    commands
+        .spawn(Collider::cuboid(16.0, 16.0))
+        .insert(TransformBundle::from(Transform::from_xyz(16.0, 64.0, 0.0)));
 
     commands.spawn((
         SpriteBundle {
